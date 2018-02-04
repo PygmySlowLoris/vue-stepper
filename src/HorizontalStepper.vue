0929742845a23164e738d542772ddbdc2b85d161
guidoec <guidoceraso@gmail.com>
2018-02-04 17:22:49 +0100

    Added unit testing

--- src/HorizontalStepper.vue
+++ src/HorizontalStepper.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="stepper-box">
         <div class="top">
-            <div class="divider-line"
-                 :style="{width: `${(100/(steps.length) * (steps.length - 1)) - 10}%`}"
-            ></div>
+            <div class="divider-line" :style="{width: `${(100/(steps.length) * (steps.length - 1)) - 10}%`}"></div>
             <div class="steps-wrapper">
                 <template v-if="topButtons">
                     <div v-if="currentStep.index > 0" class="stepper-button-top previous" @click="backStep()">
@@ -11,9 +9,7 @@
                     </div>
                 </template>
                 <template v-for="(step, index) in steps">
-                    <div :class="['step', isStepActive(index, step)]"
-                         :style="{width: `${100 / steps.length}%`}"
-                    >
+                    <div :class="['step', isStepActive(index, step)]" :key="index" :style="{width: `${100 / steps.length}%`}">
                         <div class="circle">
                             <i class="material-icons md-18">
                                 {{ (step.completed) ? 'done' : step.icon }}
@@ -31,19 +27,13 @@
             </div>
         </div>
         <div class="content">
-            <transition
-                    :enter-active-class="enterAnimation"
-                    :leave-active-class="leaveAnimation"
-                    mode="out-in"
-            >
+            <transition :enter-active-class="enterAnimation" :leave-active-class="leaveAnimation" mode="out-in">
                 <!--If keep alive-->
                 <keep-alive v-if="keepAlive">
-                    <component :is="steps[currentStep.index].component" :clickedNext="nextButton[currentStep.name]"
-                               @can-continue="proceed" @change-next="changeNextBtnValue" :current-step="currentStep"></component>
+                    <component :is="steps[currentStep.index].component" :clickedNext="nextButton[currentStep.name]" @can-continue="proceed" @change-next="changeNextBtnValue" :current-step="currentStep"></component>
                 </keep-alive>
                 <!--If not show component and destroy it in each step change-->
-                <component v-else :is="steps[currentStep.index].component" :clickedNext="nextButton[currentStep.name]"
-                           @can-continue="proceed"  @change-next="changeNextBtnValue" :current-step="currentStep"></component>
+                <component v-else :is="steps[currentStep.index].component" :clickedNext="nextButton[currentStep.name]" @can-continue="proceed" @change-next="changeNextBtnValue" :current-step="currentStep"></component>
             </transition>
         </div>
         <div :class="['bottom', (currentStep.index > 0) ? '' : 'only-next']">
@@ -60,172 +50,175 @@
 </template>
 
 <script>
-    import translations from './Translations.js'
+import translations from "./Translations.js";
 
-    export default {
-        filters: {
-            translate: function (value, locale) {
-                return translations[locale][value];
-            }
-        },
-
-        props: {
-            locale: {
-                type: String,
-                default: 'en'
-            },
-            topButtons: {
-                type: Boolean,
-                default: false
-            },
-            steps: {
-                type: Array,
-                default: function () {
-                    return [
-                        {
-                            icon: 'mail',
-                            name: 'first',
-                            title: 'Sample title 1',
-                            subtitle: 'Subtitle sample'
-
-                        },
-                        {
-                            icon: 'report_problem',
-                            name: 'second',
-                            title: 'Sample title 2',
-                            subtitle: 'Subtitle sample'
-                        }
-                    ]
-                }
-            },
-            keepAlive: {
-                type: Boolean,
-                default: true
-            }
-        },
-
-        data() {
-            return {
-                currentStep: {},
-                previousStep: {},
-                nextButton: {},
-                canContinue: false,
-                finalStep: false
-            }
-        },
-
-        computed: {
-            enterAnimation() {
-                if (this.currentStep.index < this.previousStep.index) {
-                    return 'animated quick fadeInLeft'
-                } else {
-                    return 'animated quick fadeInRight'
-                }
-            },
-            leaveAnimation() {
-                if (this.currentStep.index > this.previousStep.index) {
-                    return 'animated quick fadeOutLeft'
-                } else {
-                    return 'animated quick fadeOutRight'
-                }
-            }
-        },
-
-        methods: {
-            isStepActive(index, step) {
-                if (this.currentStep.index === index) {
-                    return 'activated'
-                } else {
-                    return 'deactivated'
-                }
-            },
-
-            activateStep(index, back = false) {
-                if (this.steps[index]) {
-                    this.previousStep = this.currentStep;
-                    this.currentStep = {
-                        name: this.steps[index].name,
-                        index: index
-                    };
-
-                    if (index + 1 === this.steps.length) {
-                        this.finalStep = true;
-                    } else {
-                        this.finalStep = false;
-                    }
-
-                    if (!back) {
-                        this.$emit('completed-step', this.previousStep);
-                    }
-                }
-                this.$emit('active-step', this.currentStep);
-            },
-
-            nextStep() {
-                this.nextButton[this.currentStep.name] = true;
-                if (this.canContinue) {
-                    if (this.finalStep) {
-                        this.$emit('stepper-finished', this.currentStep);
-                    }
-                    let currentIndex = this.currentStep.index + 1;
-
-                    this.activateStep(currentIndex);
-                }
-                this.canContinue = false;
-                this.$forceUpdate();
-            },
-
-            backStep() {
-                this.$emit('clicking-back');
-                let currentIndex = this.currentStep.index - 1;
-                if (currentIndex >= 0) {
-                    this.activateStep(currentIndex, true);
-                }
-            },
-
-            proceed(payload) {
-                this.canContinue = payload.value;
-            },
-
-            changeNextBtnValue(payload) {
-                this.nextButton[this.currentStep.name] = payload.nextBtnValue;
-                this.$forceUpdate();
-            }
-        },
-
-        created() {
-            // Initiate stepper
-            this.activateStep(0);
-            this.steps.forEach((step) => {
-                this.nextButton[step.name] = false;
-            });
-        }
+export default {
+  filters: {
+    translate: function(value, locale) {
+      return translations[locale][value];
     }
+  },
+
+  props: {
+    locale: {
+      type: String,
+      default: "en"
+    },
+    topButtons: {
+      type: Boolean,
+      default: false
+    },
+    steps: {
+      type: Array,
+      default: function() {
+        return [
+          {
+            icon: "mail",
+            name: "first",
+            title: "Sample title 1",
+            subtitle: "Subtitle sample"
+          },
+          {
+            icon: "report_problem",
+            name: "second",
+            title: "Sample title 2",
+            subtitle: "Subtitle sample"
+          }
+        ];
+      }
+    },
+    keepAlive: {
+      type: Boolean,
+      default: true
+    }
+  },
+
+  data() {
+    return {
+      currentStep: {},
+      previousStep: {},
+      nextButton: {},
+      canContinue: false,
+      finalStep: false
+    };
+  },
+
+  computed: {
+    enterAnimation() {
+      if (this.currentStep.index < this.previousStep.index) {
+        return "animated quick fadeInLeft";
+      } else {
+        return "animated quick fadeInRight";
+      }
+    },
+    leaveAnimation() {
+      if (this.currentStep.index > this.previousStep.index) {
+        return "animated quick fadeOutLeft";
+      } else {
+        return "animated quick fadeOutRight";
+      }
+    }
+  },
+
+  methods: {
+    isStepActive(index, step) {
+      if (this.currentStep.index === index) {
+        return "activated";
+      } else {
+        return "deactivated";
+      }
+    },
+
+    activateStep(index, back = false) {
+      if (this.steps[index]) {
+        this.previousStep = this.currentStep;
+        this.currentStep = {
+          name: this.steps[index].name,
+          index: index
+        };
+
+        if (index + 1 === this.steps.length) {
+          this.finalStep = true;
+        } else {
+          this.finalStep = false;
+        }
+
+        if (!back) {
+          this.$emit("completed-step", this.previousStep);
+        }
+      }
+      this.$emit("active-step", this.currentStep);
+    },
+
+    nextStep() {
+      this.nextButton[this.currentStep.name] = true;
+      if (this.canContinue) {
+        if (this.finalStep) {
+          this.$emit("stepper-finished", this.currentStep);
+        }
+        let currentIndex = this.currentStep.index + 1;
+
+        this.activateStep(currentIndex);
+      }
+      this.canContinue = false;
+      this.$forceUpdate();
+    },
+
+    backStep() {
+      this.$emit("clicking-back");
+      let currentIndex = this.currentStep.index - 1;
+      if (currentIndex >= 0) {
+        this.activateStep(currentIndex, true);
+      }
+    },
+
+    proceed(payload) {
+      this.canContinue = payload.value;
+    },
+
+    changeNextBtnValue(payload) {
+      this.nextButton[this.currentStep.name] = payload.nextBtnValue;
+      this.$forceUpdate();
+    }
+  },
+
+  created() {
+    // Initiate stepper
+    this.activateStep(0);
+    this.steps.forEach(step => {
+      this.nextButton[step.name] = false;
+    });
+  }
+};
 </script>
 
-<style src="./HorizontalStepper.scss" scoped lang="scss"></style>
-<style scoped>
-    /* fallback */
-    @font-face {
-        font-family: 'Material Icons';
-        font-style: normal;
-        font-weight: 400;
-        src: local('Material Icons'), local('MaterialIcons-Regular'), url(https://fonts.gstatic.com/s/materialicons/v17/2fcrYFNaTjcS6g4U3t-Y5ZjZjT5FdEJ140U2DJYC3mY.woff2) format('woff2');
-    }
+<style src="./HorizontalStepper.scss" scoped lang="scss">
 
-    .material-icons {
-        font-family: 'Material Icons';
-        font-weight: normal;
-        font-style: normal;
-        font-size: 24px;
-        line-height: 1;
-        letter-spacing: normal;
-        text-transform: none;
-        display: inline-block;
-        white-space: nowrap;
-        word-wrap: normal;
-        direction: ltr;
-        -webkit-font-feature-settings: 'liga';
-        -webkit-font-smoothing: antialiased;
-    }
+</style>
+<style scoped>
+/* fallback */
+@font-face {
+  font-family: "Material Icons";
+  font-style: normal;
+  font-weight: 400;
+  src: local("Material Icons"), local("MaterialIcons-Regular"),
+    url(https://fonts.gstatic.com/s/materialicons/v17/2fcrYFNaTjcS6g4U3t-Y5ZjZjT5FdEJ140U2DJYC3mY.woff2)
+      format("woff2");
+}
+
+.material-icons {
+  font-family: "Material Icons";
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  -webkit-font-feature-settings: "liga";
+  -webkit-font-smoothing: antialiased;
+}
 </style>
---
+++ test/StepTwo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Step One</h1>
+        <button @click="canContinue">Finish</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['clickedNext', 'currentStep'],
+        methods: {
+            canContinue() {
+                this.$emit('can-continue', {value: true});
+            }
+        }
+    }
+</script>
